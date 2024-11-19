dfacf3ba943938a5cddc39b3eb85a6a2a39d2f8d
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-11-19 10:56:54 +0545

    added some information

--- Research/admin.py
+++ Research/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import School, Book
+from .models import School, Book, Person
 
 admin.site.register(School)
 admin.site.register(Book)
+admin.site.register(Person)
