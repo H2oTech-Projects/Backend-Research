4d871d9a1688945f0b01e66ef2a9cb23313e1711
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-11-22 11:34:53 +0545

    Update .gitignore to exclude .env

--- Research/views.py
+++ Research/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+import os
+
+def my_view(request):
+    api_key = os.getenv('API_KEY')  # Access the API_KEY
+    return JsonResponse({"message": f"Your API key is {api_key}"})
