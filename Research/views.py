0dff24d9205c9824e0ee909c229d229be71910b0
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-11-24 14:35:47 +0545

    Basic route created

--- Research/views.py
+++ Research/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def author_list(request):
+    return JsonResponse({"message": "List of authors"})
