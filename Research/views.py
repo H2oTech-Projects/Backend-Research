c6f3512ff4197f744af5f30ba153fe05345b5a1c
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-12-03 10:49:14 +0545

    Celery dynamic tasks and crontab

--- Research/views.py
+++ Research/views.py
@@ -17,10 +17,10 @@ def send_mail_to_all(request):
 
 
 def schedule_mail(request):
-    schedule, created = CrontabSchedule.objects.get_or_create(hour=21, minute=31)
+    schedule, created = CrontabSchedule.objects.get_or_create(hour=8, minute=45)
     task = PeriodicTask.objects.create(
         crontab=schedule,
-        name="schedule_mail_task_"+"5",
+        name="schedule_mail_task_"+"1",
         task='send_mail.tasks.send_mail_task',
        # args=json.dumps([2, 3])
     )
