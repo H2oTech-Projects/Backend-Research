223e8c273eccacc849e62a3655c33ffe99d29980
Indira Kasichhwa <kasichhwaindira@gmail.com>
2025-01-05 16:31:11 +0545

    fixing errors

--- Research/admin.py
+++ Research/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Author , Author2
+'''
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+'''
+admin.site.register(Author)
+admin.site.register(Author2)
 
-# Register your models here.
--- Research/models.py
+++ Research/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField()
+    number_of_book = models.IntegerField()
+    latest_book = models.CharField()
+    date_of_publication = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+class Author2(models.Model):
+    name = models.CharField()
+    number_of_book = models.IntegerField()
+    latest_book = models.CharField()
+    date_of_publication = models.DateField()
+
+    def __str__(self):
+        return self.name
